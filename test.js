9d3fc8b0dcf9824fefcf3d9aa4dbcb8d059cfd75
amar-nath1 <amarat279955>
2022-10-22 18:04:02 +0530

    final deliverable2.2-2.3

--- test.js
+++ test.js
@@ -1,27 +1,42 @@
-let getA=(a)=>a
-console.log(getA(5))
+class Student{
 
-var b=8
-let getB=()=>b
-console.log(getB())
+    static count=0
 
-let square=(a)=>{return a*a}
+    constructor(name,age,phone_num,board_marks){
 
-console.log(square(5))
+        this.name=name
+        this.age=age
+        this.phone_num=phone_num
+        this.board_marks=board_marks
+        Student.count++
+    }
 
-var x=function(){
+   static printcount(){
+        console.log(this.count)
+    }
 
-    this.val=1
-    setTimeout(()=>{
+    checkeligibility(boardmarks){
+        var agelimit=40
+        var minmarks=35
+        let a= (age)=> {return (boardmarks>=minmarks && age<=agelimit)}
+        
+        return a
+    }
 
-        this.val++
-        console.log(this.val)
-    },1)
+
 }
-// x()
 
-var y=(...n)=>{
+const stu1=new Student('rob',24,1234,36)
+const stu2=new Student('bob',28,54334,44)
+const stu3=new Student('lalit',18,5438532,24)
+const stu4=new Student('varun',38,4638532,54)
+const stu5=new Student('vijay',484568532,74)
 
-    console.log(n[1])
-}
-y(3,5,6)
+
+Student.printcount()
+
+console.log(stu1.checkeligibility(stu1.board_marks)(stu1.age))
+console.log(stu2.checkeligibility(stu2.board_marks)(stu2.age))
+console.log(stu3.checkeligibility(stu3.board_marks)(stu3.age))
+console.log(stu4.checkeligibility(stu4.board_marks)(stu4.age))
+console.log(stu5.checkeligibility(stu5.board_marks)(stu5.age))
